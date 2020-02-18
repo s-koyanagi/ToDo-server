30e4c07701554dac0d95cc3b532f655e609320aa
s-koyanagi <kysh19931216@gmail.com>
2020-02-18 19:19:53 +0900

    minor fix

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS users (
-  id BIGSERIAL,
+CREATE TABLE IF NOT EXISTS users(
+  id SERIAL,
   full_name VARCHAR(128) NOT NULL,
   first_name VARCHAR(128) NOT NULL,
   last_name VARCHAR(128) NOT NULL,
@@ -8,4 +8,4 @@ CREATE TABLE IF NOT EXISTS users (
   admin_flag CHAR(1),
   PRIMARY KEY (id),
   UNIQUE KEY (email)
-)
+);
