4913d2503fb586a2c416c4932c8eef5a526ec8a3
s-koyanagi <kysh19931216@gmail.com>
2020-02-28 18:59:57 +0900

    minor fix

--- src/main/java/com/kysh/todo/auth/AuthService.java
+++ src/main/java/com/kysh/todo/auth/AuthService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class AuthService implements UserDetailsService {
 
     @Autowired
@@ -17,7 +19,6 @@ public class AuthService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
         String email = Optional.ofNullable(id).orElseThrow(() -> new UsernameNotFoundException("メールアドレスが未入力です。"));
-        User user = userDao.selectByEmail(email).orElseThrow(()-> new UsernameNotFoundException("入力内容が不正です。"));
-        return user;
+        return userDao.selectByEmail(email).orElseThrow(()-> new UsernameNotFoundException("入力内容が不正です。"));
     }
 }
