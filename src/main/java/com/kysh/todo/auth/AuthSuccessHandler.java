435108a112dda76b84b8d047c64f7d1b1c68817c
s-koyanagi <kysh19931216@gmail.com>
2020-03-26 17:22:16 +0900

    認証成功時にユーザ情報を返却するように修正

--- src/main/java/com/kysh/todo/auth/AuthSuccessHandler.java
+++ src/main/java/com/kysh/todo/auth/AuthSuccessHandler.java
@@ -1,7 +1,16 @@
 package com.kysh.todo.auth;
 
+import com.kysh.todo.domain.dto.UserDto;
+import com.kysh.todo.domain.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -9,19 +18,34 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Component
 public class AuthSuccessHandler implements AuthenticationSuccessHandler {
 
+    @Autowired
+    MappingJackson2HttpMessageConverter httpMessageConverter;
+
+    private static final MediaType CONTENT_TYPE_JSON = MediaType.APPLICATION_JSON;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        HttpOutputMessage outputMessage = new ServletServerHttpResponse(response);
+        User user = (User)authentication.getPrincipal();
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFullName(user.getFullName());
+        userDto.setFirstName(user.getFirstName());
+        userDto.setLastName(user.getLastName());
+        userDto.setEmail(user.getEmail());
+        userDto.setAdminFlag(user.getAdminFlag());
+        httpMessageConverter.write(userDto, CONTENT_TYPE_JSON, outputMessage);
         response.setStatus(HttpStatus.OK.value());
         clearAuthenticationAttributes(request);
     }
 
     private void clearAuthenticationAttributes(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
-
         if (session == null) {
             return;
         }
