5631d46079cf2a138b573fb937bcd4ec9a8b97c3
s-koyanagi <kysh19931216@gmail.com>
2021-02-05 23:48:31 +0900

    add tasks table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,19 @@
 CREATE TABLE IF NOT EXISTS users(
-  id SERIAL,
-  full_name VARCHAR(128) NOT NULL,
-  first_name VARCHAR(128) NOT NULL,
-  last_name VARCHAR(128) NOT NULL,
-  password text NOT NULL,
-  email VARCHAR(256) NOT NULL,
-  admin_flag CHAR(1),
-  PRIMARY KEY (id),
-  UNIQUE KEY (email)
+  id SERIAL
+  ,full_name VARCHAR(128) NOT NULL
+  ,first_name VARCHAR(128) NOT NULL
+  ,last_name VARCHAR(128) NOT NULL
+  ,password text NOT NULL
+  ,email VARCHAR(256) NOT NULL
+  ,admin_flag CHAR(1)
+  ,PRIMARY KEY (id)
+  ,UNIQUE KEY (email)
 );
+
+CREATE TABLE IF NOT EXISTS tasks(
+  id SERIAL
+  ,project_id integer
+  ,subject VARCHAR(128) NOT NULL
+  ,status_Id integer
+  ,dead_line date
+)
